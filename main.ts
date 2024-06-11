f521a2b24819026f1ff86b9cf2ee152fa1aeb585
Mary-melvin <162853197+Mary-melvin@users.noreply.github.com>
2024-06-11 23:14:20 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,34 @@
+
+import inquirer from "inquirer";
+//Asking questions from users through inquirer
+let answers = await inquirer.prompt([
+    {message: "Enter First Number", type: "number", name: "firstNumber"},
+    {message: "Enter Second Number", type: "number", name: "secondNumber"},
+    {
+
+        message: "Select one operator to perform Operations",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Substraction", "Multiplication", "Division"],
+    },
+]);
+
+// contional statements if-Else
+if(answers.operator === "Addition"){
+    console.log(answers.firstNumber + answers.secondNumber)
+}
+else if(answers.operator === "Substraction"){
+    console.log(answers.firstNumber - answers.secondNumber)
+}
+
+else if(answers.operator === "Multiplication"){
+    console.log(answers.firstNumber * answers.secondNubmer)
+}
+
+else if (answers.operator === "Division"){
+    console.log(answers.firstNumber / answers.secondNumber)
+}
+
+else{
+    console.log("Invalid Input")
+}
